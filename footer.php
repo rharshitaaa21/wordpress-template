2a4d3bf399841367be305bf2d988ffaaf4e03b3f
rharshitaaa21 <raghav.harshita21@gmail.com>
2023-05-03 12:54:54 +0530

    Updated File

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+<?php
+        wp_footer();
+    ?>
+    <!-- Bootstrap Javascript -->          
+</body>
+</html> 
+
--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 
 <?php
 
-function add_css()
+function followandrew_register_styles()
 
 {
     $version = wp_get_theme()->get('Version');
@@ -17,4 +17,23 @@ function add_css()
     array(),'5.13.0','all');
 }
 
-add_action('wp_enqueue_scripts', 'add_css');
+add_action('wp_enqueue_scripts', 'followandrew_register_styles');
+
+
+function followandrew_register_script()
+
+{
+    $version = wp_get_theme()->get('Version');
+    
+    wp_enqueue_script('followandrew-mainn', get_template_directory_uri() . '/assets/js/main.js', 
+    array(),$version,'all');
+    wp_enqueue_script('followandrew-bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js',
+    array(), '4.4.1', true );
+    wp_enqueue_script('followandrew-popper','https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
+    array(), '1.16.0', true );
+    wp_enqueue_script('followandrew-jquery','https://code.jquery.com/jquery-3.4.1.slim.min.js',
+    array(), '3.4.1', true );
+
+}
+
+add_action('wp_enqueue_scripts', 'followandrew_register_script');
