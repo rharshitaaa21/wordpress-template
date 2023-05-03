0c111e351a5efd3154f916be47152b5c58b1353d
rharshitaaa21 <raghav.harshita21@gmail.com>
2023-05-03 11:44:15 +0530

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+
+<?php
+
+//function add_css()
+
+//{
+
+  //  wp_enqueue_style('first', get_template_directory_uri() . '/assets/css/style.css', false,'1.0','all');
+
+//    wp_enqueue_style( 'first');
+
+//}
+
+//add_action('wp_enqueue_scripts', 'add_css');
